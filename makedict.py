17f5fbd3dc9c8f6e6d6025a7e66d130cd8bde46b
hamiltonne <sil.hamilton@gmail.com>
2019-04-21 20:29:29 -0400

    Initial project upload.

---
+++ makedict.py
@@ -0,0 +1,16 @@
+import string
+dictionary = []
+alphabet = list(string.ascii_uppercase)
+for letter in alphabet:
+    file = open("gcide-0.52/CIDE." + letter).read().split("\n")
+    for line in file:
+        if line[:8] == "<p><ent>":
+            if line[8:-10].lower() not in dictionary:
+                dictionary.append(line[8:-10].lower())
+out = open("dictionary.txt", "w")
+for word in dictionary:
+    out.write(word)
+    out.write("\n")
+out.close()
+print("Exported to dictionary.txt")
+quit()
